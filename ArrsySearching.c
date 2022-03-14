efb94b47ff4bfb71e2724675d4ad9cd29f17a924
Athif binu <binuathif@gmail.com>
2022-03-14 19:00:10 +0530

    new file add in array in searching

---
+++ ArrsySearching.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int limit,i,values[100],j,temp;
+	printf("Enter a limit");
+	scanf("%d",&limit);
+	printf("Enter a values");
+	for(i=0;i<limit;i++){
+		scanf("%d",&values[i]);
+	}
+     for(i=0;i<limit-1;i++){
+    	 for(j=i+1;j<limit;j++){
+    		 if(values[i]>values[j]){
+    			 temp=values[i];
+    			 values[i]=values[j];
+    			  values[j]=temp;
+
+    		 }
+
+    	 }
+     }
+
+	for(i=0;i<limit;i++){
+		printf("%d\t",values[i]);
+	}
+	return EXIT_SUCCESS;
+}
