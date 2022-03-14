a13067f9bc2433df3f8fc4dfd5e4845daae10de0
Athif binu <binuathif@gmail.com>
2022-03-14 15:17:53 +0530

    error change

---
+++ piramid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n,i,j;
+	printf("enter Your size");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=2*n-1;j++){
+			if(j>=n-(i-1) && j<=n+(i-1)){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	
+
+
+	return EXIT_SUCCESS;
+
+
+}
+
